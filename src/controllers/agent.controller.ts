fc84891198b7c1d034f81cc5b46c5a563eadfbfa
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-10-02 03:26:04 +0900

    feat: develop admin frontend and integrate header & footer

--- src/controllers/agent.controller.ts
+++ src/controllers/agent.controller.ts
@@ -10,7 +10,7 @@ const memberService = new MemberService();
 agentController.goHome = (req: Request, res: Response) => {
     try {
         console.log("goHome");
-        res.send("Home Page");
+        res.render("home");
     } catch (err) {
         console.log("Error, goHome:", err);
     }
@@ -19,7 +19,7 @@ agentController.goHome = (req: Request, res: Response) => {
 agentController.getSignup = (req: Request, res: Response) => {
     try {
         console.log("getSignup");
-        res.send("Signup Page");
+        res.render("signup");
     } catch (err) {
         console.log("Error, getSignup:", err);
     }
@@ -28,7 +28,7 @@ agentController.getSignup = (req: Request, res: Response) => {
 agentController.getLogin = (req: Request, res: Response) => {
     try {
         console.log("getLogin");
-        res.send("Login Page");
+        res.render("login");
     } catch (err) {
         console.log("Error, getLogin:", err);
     }
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,9 @@ mongoose
         console.log("MongoDB connection succeed");
         const PORT = process.env.PORT ?? 3003 ;
         app.listen(PORT, function(){
-            console.log(`The server is running successfully on port: ${PORT}`);
+            console.info(`The server is running successfully on port: ${PORT}`);
+            console.info(`Admin project on http://localhost:${PORT}/admin \n`);
+            
         });
     })
     .catch(err => console.log("ERROR on connection MongoDB", err));
