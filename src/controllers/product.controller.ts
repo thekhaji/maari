407dc75146e7124962a84a2683be1497b1e0c6f7
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-10-02 16:49:38 +0900

    feat: create product controller and routing apis

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,39 @@
+import Errors from "../libs/Errors";
+import {Response, Request} from "express";
+import { T } from "../libs/types/common";
+import { Message } from "../libs/Errors";
+
+const productController: T = {};
+productController.getAllProducts = async (req: Request, res: Response) => {
+    try {
+        console.log("getAllProducts");
+        res.render("products");
+    } catch (err) {
+        console.log("Error, getAllProducts:", err);
+        const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_RONG;
+        res.send(`<script> alert("${message}"); window.location.replace('admin/login)</script>`);
+    }
+}
+
+productController.createNewProduct = async (req: Request, res: Response) => {
+    try {
+        console.log("createNewProduct");
+        
+    } catch (err) {
+        console.log("Error, createNewProduct:", err);
+        const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_RONG;
+        res.send(`<script> alert("${message}"); window.location.replace('admin/login)</script>`);
+    }
+}
+
+productController.updateChosenProduct = async (req: Request, res: Response) => {
+    try {
+        console.log("updateChosenProduct");
+        
+    } catch (err) {
+        console.log("Error, updateChosenProduct:", err);
+        const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_RONG;
+        res.send(`<script> alert("${message}"); window.location.replace('admin/login)</script>`);
+    }
+}
+export default productController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import agentController from "./controllers/agent.controller";
+import productController from "./controllers/product.controller";
 
 const routerAdmin = express.Router();
 
@@ -11,12 +12,14 @@ routerAdmin
 routerAdmin
     .get("/signup", agentController.getSignup)
     .post("/signup", agentController.processSignup);
-routerAdmin
-    .get("/logout", agentController.logout)
-routerAdmin
-    .get("/checkAuthSession", agentController.checkAuthSession)
+routerAdmin.get("/logout", agentController.logout)
+routerAdmin.get("/checkAuthSession", agentController.checkAuthSession)
 
 /** Product **/
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/update", productController.updateChosenProduct);
+
 /** User **/
 
 export default routerAdmin;
