4dc6082c2829fcd14a76ee820f03c10991c2da3a
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-10-04 17:07:44 +0900

    feat: signup process validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,20 @@
 console.log("Signup frontend javascript file");
+
+function validateSignupForm(){
+    const memberNick = $(".member-nick").val(),
+    memberPhone = $(".member-phone").val(),
+    memberPassword = $(".member-password").val(),
+    confirmPassword = $(".confirm-password").val();
+    
+    if (memberNick === "" || memberPhone === "" || memberPassword === "" || confirmPassword === ""){
+        alert("Please insert all the required fields!");
+        return false;
+    }
+
+    if(memberPassword !== confirmPassword){
+        alert("Password differs, please check!");
+        return false;  
+    }   
+
+
+}
