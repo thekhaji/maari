eb5742dc00f8ed0df63170ba9c61099298b1b6a0
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-10-05 01:47:58 +0900

    feat: updateChosenUser frontend logic develop

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,28 @@
 console.log("Users frontend javascript file");
+$(function(){
+    $(".member-status").on("change", function(e){
+        const  id = e.target.id;
+
+
+        const memberStatus = $(`#${id}.member-status`).val();
+
+        axios
+            .post("/admin//user/edit", {
+                _id: id, 
+                memberStatus: memberStatus
+            })
+            .then((response)=>{
+                console.log("response:", response);
+                const result = response.data;
+
+                if(result.data){
+                    $(".member-status").blur();
+                }
+                 
+            })
+            .catch((err)=>{
+                alert("User update failed!");
+            });
+    })
+    
+});
