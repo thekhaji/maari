b697e390261c26b36d6c1776aab965222d7af043
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-10-22 17:18:31 +0900

    fix: fixing some product related bugs

--- src/router.ts
+++ src/router.ts
@@ -23,6 +23,6 @@ router.get("/product/:id", memberController.retrieveAuth, productController.getP
 /** ORDER **/
 router.post("/order/create", memberController.verifyAuth, orderController.createOrder);
 router.get("/order/all", memberController.verifyAuth, orderController.getMyOrders);
-router.get("/order/update", memberController.verifyAuth, orderController.updateOrder);
+router.post("/order/update", memberController.verifyAuth, orderController.updateOrder);
 
 export default router;
